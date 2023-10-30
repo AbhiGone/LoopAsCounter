b25fc78635e0cb9304ced7c4980ee700f95a6c77
AbhiGone <giriabhinaya@gmail.com>
2023-10-30 09:21:52 -0400

    counted

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for (int j = 30; j >= 0; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        for (int k = 0; k <= 18; k += 3) {
+            System.out.print(k + " ");
+
+        }
+        System.out.println();
+
+        for(int o = 10; o >= 0; o -= 2) {
+            System.out.print(o + " ");
+        }
+    }
+}
